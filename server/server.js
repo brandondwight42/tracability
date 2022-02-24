1936ffc0b94ee21838b5e97cee6416b1cadc5f98
Brandon Hunsaker <brandondwight42@gmail.com>
2022-02-24 15:50:28 -0700

    warning added

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ try {nonExistentFunctionTwo();
 
 try {nonExistentFunctionThree(); 
 } catch (warning) {
-        rollbar.critical(`Warning`);     
+        rollbar.warning(`Warning`);     
         // expected output: ReferenceError: nonExistentFunction is not defined     
         // Note - error messages will vary depending on browser 
         }
